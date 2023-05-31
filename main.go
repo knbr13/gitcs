81b2bc1684a1cfc1b9a8aa42f7bbabe81e1e7e8c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-31 23:34:14 +0300

    add add and email flags

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"flag"
+)
+
+func main() {
+	var folder string
+	var email string
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+	flag.Parse()
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+
+	stats(email)
+}

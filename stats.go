aed9246742734a547a6aacef6fe1c3d86321fad2
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-31 23:18:29 +0300

    implement buildCols function

--- stats.go
+++ stats.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"log"
-	"time"
-	"sort"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"log"
+	"sort"
+	"time"
 )
 
 const outOfRange = 99999
 const daysInLastSixMonths = 183
 const weeksInLastSixMonths = 26
+
+type column []int
 
 // fillCommits given a repository found in `path`, gets the commits and
 // puts them in the `commits` map, returning it when completed
@@ -123,13 +125,36 @@ func processRepositories(email string) map[int]int {
 
 // sortMapIntoSlice returns a slice of indexes of a map, ordered
 func sortMapIntoSlice(m map[int]int) []int {
-    // order map
-    // To store the keys in slice in sorted order
-    var keys []int
-    for k := range m {
-        keys = append(keys, k)
-    }
-    sort.Ints(keys)
+	// order map
+	// To store the keys in slice in sorted order
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
 
-    return keys
+	return keys
+}
+
+// buildCols generates a map with rows and columns ready to be printed to screen
+func buildCols(keys []int, commits map[int]int) map[int]column {
+	cols := make(map[int]column)
+	col := column{}
+
+	for _, k := range keys {
+		week := int(k / 7) //26,25...1
+		dayinweek := k % 7 // 0,1,2,3,4,5,6
+
+		if dayinweek == 0 { //reset
+			col = column{}
+		}
+
+		col = append(col, commits[k])
+
+		if dayinweek == 6 {
+			cols[week] = col
+		}
+	}
+
+	return cols
 }

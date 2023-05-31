44a3340eb84acf15f31f398335f2631cb171d9ed
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-31 23:20:28 +0300

    implement printCell function

--- stats.go
+++ stats.go
@@ -4,6 +4,7 @@ import (
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"log"
+	"fmt"
 	"sort"
 	"time"
 )
@@ -157,4 +158,37 @@ func buildCols(keys []int, commits map[int]int) map[int]column {
 	}
 
 	return cols
+}
+
+// printCell given a cell value prints it with a different format
+// based on the value amount, and on the `today` flag.
+func printCell(val int, today bool) {
+	escape := "\033[0;37;30m"
+	switch {
+	case val > 0 && val < 5:
+		escape = "\033[1;30;47m"
+	case val >= 5 && val < 10:
+		escape = "\033[1;30;43m"
+	case val >= 10:
+		escape = "\033[1;30;42m"
+	}
+
+	if today {
+		escape = "\033[1;37;45m"
+	}
+
+	if val == 0 {
+		fmt.Printf(escape + "  - " + "\033[0m")
+		return
+	}
+
+	str := "  %d "
+	switch {
+	case val >= 10:
+		str = " %d "
+	case val >= 100:
+		str = "%d "
+	}
+
+	fmt.Printf(escape+str+"\033[0m", val)
 }

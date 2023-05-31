d4d439c96cbe302db45f96fd068c932635bb72ab
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-31 16:51:18 +0300

    search for '.git' folders in 'Documents' directory

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"flags"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-func main(){
-	
+func main() {
+	root := "C:\\Users\\superComputer\\Documents" // Replace with the absolute root directory path
+
+	var gitFolders []string
+
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Error accessing path %q: %v\n", path, err)
+			return nil
+		}
+
+		if info.IsDir() && info.Name() == ".git" {
+			gitFolder := filepath.Dir(path)
+			gitFolders = append(gitFolders, gitFolder)
+			return filepath.SkipDir // Skip further traversal within this directory
+		}
+
+		// Skip node_modules directories
+		if info.IsDir() && strings.ToLower(info.Name()) == "node_modules" {
+			return filepath.SkipDir
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking the path %q: %v\n", root, err)
+	} else {
+		fmt.Println("Folders containing .git:")
+		for _, folder := range gitFolders {
+			fmt.Println(folder)
+		}
+	}
 }

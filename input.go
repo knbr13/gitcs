fe1a1ca9d4aa3fbd1644349c6115776e141ba5a6
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-24 02:16:23 +0300

    implement 'getEmailFromUser' func

--- input.go
+++ input.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
+
+func getEmailFromUser(reader *bufio.Reader) string {
+	for {
+		fmt.Print("Enter your email address: ")
+		email, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		email = strings.TrimSpace(email)
+
+		if isValidEmail(email) {
+			return email
+		}
+
+		fmt.Println("Invalid email address. Please try again.")
+	}
+}
 
 func isValidEmail(email string) bool {
 	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)

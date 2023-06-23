f3c010d70c9222373346ae3741674aba08f9880b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-24 02:01:53 +0300

    implement 'isValidEmail' func

---
+++ input.go
@@ -0,0 +1,8 @@
+package main
+
+import "regexp"
+
+func isValidEmail(email string) bool {
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(email)
+}

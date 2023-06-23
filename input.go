623eacf839c662677c860e2996e49a1013513875
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-24 02:05:24 +0300

    implement 'isValidFolderPath' func

--- input.go
+++ input.go
@@ -1,8 +1,25 @@
 package main
 
-import "regexp"
+import (
+	"log"
+	"os"
+	"regexp"
+)
 
 func isValidEmail(email string) bool {
 	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	return emailRegex.MatchString(email)
+}
+
+func isValidFolderPath(folder string) bool {
+	// Check if the folder exists and is a directory
+	info, err := os.Stat(folder)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		log.Fatal(err)
+	}
+
+	return info.IsDir()
 }

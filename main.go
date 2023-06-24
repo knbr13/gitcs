9c763aa6897fe86c7558f1facefd30eedd671065
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-24 04:27:14 +0300

    delete 'gogitlocalstats' at the end of the program

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
 	email, folder := getInputFromUser()
 
@@ -8,4 +12,10 @@ func main() {
 	}
 
 	stats(email)
+
+	// There is no need to handle the errors, it is okay to keep the file in the home dir
+	homeDir, err := os.UserHomeDir()
+	if err == nil {
+		deleteFile(".gogitlocalstats", homeDir)
+	}
 }

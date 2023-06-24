1592cf4aa06b15e88c5196c42e4e750fbaebd99d
AAVision <75311052+AAVision@users.noreply.github.com>
2023-06-24 15:21:31 +0300

    Adding a spinner to the terminal

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"github.com/briandowns/spinner"
+	"time"
 )
 
 func main() {
@@ -11,8 +13,14 @@ func main() {
 		scan(folder)
 	}
 
+	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond) 
+	s.Color("red", "bold")
+	s.FinalMSG = "Done!"
+	s.Start()
+	
 	stats(email)
 
+	s.Stop()
 	// There is no need to handle the errors, it is okay to keep the file in the home dir
 	homeDir, err := os.UserHomeDir()
 	if err == nil {

922b0a887b03120193cc56972f0f3e244dfdd2b7
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-28 09:53:13 +0300

    skip the current fillCommit on error

--- stats.go
+++ stats.go
@@ -60,18 +60,21 @@ func fillCommits(email string, path string, commits map[int]int) map[int]int {
 	// instantiate a git repo object from path
 	repo, err := git.PlainOpen(path)
 	if err != nil {
-		panic(err)
+		return commits
 	}
+
 	// get the HEAD reference
 	ref, err := repo.Head()
 	if err != nil {
-		panic(err)
+		return commits
 	}
+
 	// get the commits history starting from HEAD
 	iterator, err := repo.Log(&git.LogOptions{From: ref.Hash()})
 	if err != nil {
-		panic(err)
+		return commits
 	}
+
 	// iterate the commits
 	offset := calcOffset()
 	err = iterator.ForEach(func(c *object.Commit) error {
@@ -88,7 +91,7 @@ func fillCommits(email string, path string, commits map[int]int) map[int]int {
 		return nil
 	})
 	if err != nil {
-		panic(err)
+		return commits
 	}
 
 	return commits

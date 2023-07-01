10d0ae8c6d32b4e7de943b2cb18c990642b4633b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-01 18:05:17 +0300

    move file utility funcs to fileutils.go

--- fileutils.go
+++ fileutils.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func deleteFile(fileName, filePath string) error {
@@ -30,4 +33,34 @@ func createFileIfNotExist(fileName string, dir string) error {
 	}
 
 	return nil
+}
+
+func isValidFolderPath(folder string) bool {
+	// Check if the folder exists and is a directory
+	info, err := os.Stat(folder)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+		log.Fatal(err)
+	}
+
+	return info.IsDir()
+}
+
+func getFolderFromUser(reader *bufio.Reader) string {
+	for {
+		fmt.Print("Enter the folder path to scan for Git repositories: ")
+		folder, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		folder = strings.TrimSpace(folder)
+
+		if isValidFolderPath(folder) {
+			return folder
+		}
+
+		fmt.Println("Invalid folder path. Please try again.")
+	}
 }
--- input.go
+++ input.go
@@ -94,37 +94,7 @@ func getEmailFromUser(reader *bufio.Reader) string {
 	}
 }
 
-func getFolderFromUser(reader *bufio.Reader) string {
-	for {
-		fmt.Print("Enter the folder path to scan for Git repositories: ")
-		folder, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-		folder = strings.TrimSpace(folder)
-
-		if isValidFolderPath(folder) {
-			return folder
-		}
-
-		fmt.Println("Invalid folder path. Please try again.")
-	}
-}
-
 func isValidEmail(email string) bool {
 	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
 	return emailRegex.MatchString(email)
-}
-
-func isValidFolderPath(folder string) bool {
-	// Check if the folder exists and is a directory
-	info, err := os.Stat(folder)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-		log.Fatal(err)
-	}
-
-	return info.IsDir()
 }

d832ae457eddd84721e99e559d3a046ebf21b5bb
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-12-07 17:39:56 +0200

    return the collected repos instead of writing to a file

--- fileutils.go
+++ fileutils.go
@@ -5,37 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/gookit/color"
 )
-
-func deleteFile(fileName, filePath string) error {
-	err := os.Remove(filepath.Join(filePath, fileName))
-	if err != nil {
-		return fmt.Errorf("failed to delete %s file: %s", fileName, err)
-	}
-
-	return nil
-}
-
-func createFileIfNotExist(fileName string, dir string) error {
-
-	filePath := filepath.Join(dir, fileName)
-
-	// Check if the file exists
-	_, err := os.Stat(filePath)
-	if os.IsNotExist(err) {
-		// Create the file if it doesn't exist
-		_, err := os.Create(filePath)
-		if err != nil {
-			return fmt.Errorf("failed to create file: %s", err)
-		}
-	}
-
-	return nil
-}
 
 func isValidFolderPath(folder string) bool {
 	// Check if the folder exists and is a directory

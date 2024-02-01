cf6feeb0a5d5a99c168ca8e2b6dc43cb9b79bf24
knbr13 <abdullahalaadine63@gmail.com>
2024-02-02 01:11:49 +0200

    update the spinner functionality

--- main.go
+++ main.go
@@ -14,16 +14,21 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	folder := getPathFromUser(reader)
 
-	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond, spinner.WithSuffix("  loading..."))
+	s := spinner.New(spinner.CharSets[6], 100*time.Millisecond, spinner.WithSuffix(" loading..."))
 
-	s.Color("red", "bold")
+	s.Color("green")
 	s.Start()
 	defer s.Stop()
 
 	repos, err := scanGitFolders(folder)
 	if err != nil {
-		fmt.Fprint(os.Stderr, color.Red.Sprintf("Error: %v\n", err))
+		fmt.Fprint(os.Stderr, color.Red.Sprintf("\nerror: %s\n", err.Error()))
+		s.Stop()
 		os.Exit(1)
 	}
-	stats(email, repos)
+
+	commits := processRepos(repos, email)
+	fmt.Print("\n\n")
+	printTable(commits)
+	fmt.Print("\n\n")
 }

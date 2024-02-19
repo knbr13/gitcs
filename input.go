bf670860b1d03f981e9ae47930317e5dc1352ef8
knbr13 <abdullahalaadine63@gmail.com>
2024-02-19 08:04:12 +0200

    return error instead of log.Fatal

--- input.go
+++ input.go
@@ -3,23 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/gookit/color"
 )
 
-func getPathFromUser(reader *bufio.Reader) string {
+func getPathFromUser(reader *bufio.Reader) (string, error) {
 	for {
 		fmt.Print("enter the folder path to scan for Git repositories: ")
 		input, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "gitcs: error reading input: %s\n", err.Error())
-			os.Exit(1)
+			return "", err
 		}
 		input = strings.TrimSpace(input)
 		if isValidFolderPath(input) {
-			return input
+			return input, nil
 		}
 		fmt.Println(color.Yellow.Sprintf("gitcs: path %q is not found, please enter a valid folder path", input))
 	}

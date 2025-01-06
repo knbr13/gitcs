dc88fb1ade22ff81c43b28ee024e5c383d17ed9d
knbr13 <abdullahalaadine63@gmail.com>
2025-01-06 13:49:20 +0200

    remove redundant error check

--- scan.go
+++ scan.go
@@ -6,6 +6,31 @@ import (
 	"strings"
 )
 
+func scanGitFolders(root string) ([]string, error) {
+	var gitFolders []string
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() && d.Name() == ".git" {
+			gitFolder := filepath.Dir(path)
+			gitFolders = append(gitFolders, gitFolder)
+			return filepath.SkipDir // Skip further traversal within this directory
+		}
+
+		// Skip dependency directories
+		_, ok := excludedFolders[strings.ToLower(d.Name())]
+		if d.IsDir() && ok {
+			return filepath.SkipDir
+		}
+
+		return nil
+	})
+
+	return gitFolders, err
+}
+
 // excludedFolders is a map of folder names (case-insensitive) to be excluded during the scan.
 var excludedFolders = map[string]struct{}{
 	"node_modules": {},
@@ -39,32 +64,4 @@ var excludedFolders = map[string]struct{}{
 	".gitlab":      {},
 	".vagrant":     {},
 	".serverless":  {},
-}
-
-func scanGitFolders(root string) ([]string, error) {
-	var gitFolders []string
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() && d.Name() == ".git" {
-			gitFolder := filepath.Dir(path)
-			gitFolders = append(gitFolders, gitFolder)
-			return filepath.SkipDir // Skip further traversal within this directory
-		}
-
-		// Skip dependency directories
-		_, ok := excludedFolders[strings.ToLower(d.Name())]
-		if d.IsDir() && ok {
-			return filepath.SkipDir
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-	return gitFolders, nil
 }

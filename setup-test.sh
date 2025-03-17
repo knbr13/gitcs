ed43b46d06260c7eadf692805da183711218da40
knbr13 <abdullahalaadine63@gmail.com>
2025-03-17 13:29:19 +0200

    create more test data

--- setup-test.sh
+++ setup-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+wd=$PWD
 projects=("project_1" "project_2" "project_3")
 
 for project in "${projects[@]}"; do
@@ -17,5 +18,28 @@ for project in "${projects[@]}"; do
         git commit -m "Commit $i"
     done
 
-    cd ../../ || exit
+    cd "$wd" || exit
 done
+
+cd "$wd" || exit
+
+mkdir -p "test_data/project_that_has_future_commits"
+cd "test_data/project_that_has_future_commits" || exit
+git init
+git config user.name "tester"
+git config user.email "tester@test.com"
+echo "some content" > "text.txt"
+git add "text.txt"
+commit_date=$(date -d "+1 year" --iso-8601=seconds)
+git commit --date="$commit_date" -m "commit message"
+
+cd "$wd" || exit
+
+mkdir -p "test_data/project_by_another_contributor"
+cd "test_data/project_by_another_contributor" || exit
+git init
+git config user.name "another_tester"
+git config user.email "another_tester@test.com"
+echo "Content added by another contributor" > "another_file.txt"
+git add "another_file.txt"
+git commit -m "Commit by another contributor"
